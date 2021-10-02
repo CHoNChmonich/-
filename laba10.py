3e18376de4b6ec5cbc606844e127be541248e99f
CHoNChmonich <90987051+CHoNChmonich@users.noreply.github.com>
2021-10-02 21:20:31 +0300

    Update laba10.py

--- laba10.py
+++ laba10.py
@@ -3,19 +3,31 @@ def f(a,b):
     if(a>2)and(b<=3):
         return True
     return False
+print("Введите значение для переменной а:",end="")
 a=int(input())
+print("Введите значение для переменной b:",end="")
 b=int(input())
-print(f(a,b))
+if(f(a,b)):
+    print("Высказывание истинно")
+else:
+    print("Высказывание ложно")
 #2
 def f(a,b,c):
     if(a<b)and(b<c):
         return True
     return False
+print("Введите значение для переменной а:",end="")
 a=int(input())
+print("Введите значение для переменной b:",end="")
 b=int(input())
+print("Введите значение для переменной c:",end="")
 c=int(input())
-print(f(a,b,c))
+if(f(a,b,c)):
+    print("Высказывание истинно")
+else:
+    print("Высказывание ложно")
 #3
+print("Введите число:",end="")
 x=int(input())
 def f(x):
     c=0
@@ -25,8 +37,12 @@ def f(x):
     if(c==2):
         return True
     return False
-print(f(x))
+if(f(x)):
+    print("Высказывание истинно")
+else:
+    print("Высказывание ложно")
 #4
+print("Введите число:",end="")
 x=input()
 x1=int(x[0])
 x2=int(x[1])
@@ -35,8 +51,12 @@ def f(a,b,c):
     if((a<b)and(b<c))or((a>b)and(b>c)):
         return True
     return False
-print(f(x1,x2,x3))
+if(f(x1,x2,x3)):
+    print("Высказывание истинно")
+else:
+    print("Высказывание ложно")
 #5
+print("Введите число:",end="")
 x=input()
 x1=int(x[0])
 x2=int(x[1])
@@ -47,19 +67,28 @@ if(x1==x4)and(x2==x3):
 else:
     print("False")
 #6
+print("Введите значение для стороны треугольника №1:",end="")
 a=int(input())
+print("Введите значение для стороны треугольника №2:",end="")
 b=int(input())
+print("Введите значение для стороны треугольника №3:",end="")
 c=int(input())
 if(a**2+b**2==c**2)or(a**2+c**2==b**2)or(b**2+c**2==a**2):
-    print("True")
+    print("Высказывание истинно")
 else:
-    print("False")
+    print("Высказывание ложно")
 #7
+print("Введите значение для стороны треугольника №1:",end="")
 a=int(input())
+print("Введите значение для стороны треугольника №2:",end="")
 b=int(input())
+print("Введите значение для стороны треугольника №3:",end="")
 c=int(input())
 def f(a,b,c):
     if(a+b>c)or(a+c>b)or(b+c>a):
         return True
     return False
-print(f(a,b,c))
+if(f(a,b,c)):
+    print("Высказывание истинно")
+else:
+    print("Высказывание ложно")
